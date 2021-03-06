5dd46ac044d843a1afa502fd00df2798d6587479
muhamed gündoğan <rey_36_63@hotmail.com>
2021-03-06 14:18:03 +0300

    Proje dosyası ekle.

---
+++ siralisayi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siralisayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int[] sayilar = new int[sayi];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+              x=  rnd.Next(0, 5);
+                for (int k = 0; k < i; k++)
+                {
+                    if (sayilar[i]==sayilar[k])
+                    {
+                        i--;
+                        break;
+                    }
+
+                }
+            }
+            Array.Sort(sayilar);
+            Console.WriteLine(sayilar);
+            Console.ReadKey();
+        }
+    }
+}
